ed13c42c347dbc735405eac9acff17424ec89953
Richard Riss <richard.riss@desy.de>
2023-01-12 16:47:57 +0100

    Uebung 11

--- Uebung11/src/ex11.erl
+++ Uebung11/src/ex11.erl
@@ -13,7 +13,8 @@
 -export([
         setup/1,
         bully/0,
-        bully/2
+        bully/3,
+        counter/0
         ]).
 
 
@@ -25,10 +26,14 @@ setup(NumProcesses)->
   setup(NumProcesses,[]).
 
 setup(0, PidProcesses) ->
+  PidCounter = spawn(?MODULE, counter,[]),
   lists:map(fun(Pid) ->
-    rpc(Pid,{group, PidProcesses})
+    rpc(Pid,{group, PidProcesses, PidCounter})
             end, PidProcesses),
-  start_election(PidProcesses);
+  start_election(PidProcesses),
+  timer:sleep(500),
+  get_final_coordinator(PidProcesses),
+  get_final_counter(PidCounter);
 setup(NumProcesses, PidProcesses)->
   Pid = spawn(?MODULE, bully, []),
   setup(NumProcesses - 1, lists:append(PidProcesses,[Pid])).
@@ -41,43 +46,56 @@ setup(NumProcesses, PidProcesses)->
 bully() ->
   io:format("Bully started with election value ~p. ~n", [self()]),
   receive
-    {PidSender, {group, PidProcesses}} ->
+    {PidSender, {group, PidProcesses, PidCounter}} ->
       PidSender ! ok,
-      bully(0, PidProcesses)
+      bully(0, PidProcesses, PidCounter)
   end.
 
-bully(PidCoordinator, Processes) ->
+bully(PidCoordinator, Processes, PidCounter) ->
   SelflessList = lists:filter(fun(X) -> X /= self() end, Processes),
   receive
     {PidSender, election} ->
-      PidSender ! ok,
-      FilteredList = lists:filter(fun(X) -> X > self() end, Processes),
+      PidCounter ! {PidSender, election},
+      PidSender ! {self(),alive},
+      % Find bigger Election values
+      GreaterPidList = lists:filter(fun(X) -> X > self() end, Processes),
+      % Request election from those PIDs
+      AnswerList = lists:map(fun(Pid) ->
+          rpc(Pid, election)
+      end, GreaterPidList),
+      % Filter for alive Processes
+      FilteredAnswerList = lists:filter(fun(X) -> X /= unreachable end, AnswerList),
       if
-        length(FilteredList) == 0 ->
+        length(FilteredAnswerList) > 0 ->
+          bully(PidCoordinator, Processes, PidCounter);
+        true ->
           lists:foreach(fun(Pid) ->
             Pid ! {coordinator, self()}
           end, SelflessList),
-          bully(self(), Processes);
-        true ->
-          AnswerList = lists:map(fun(Pid) ->
-            rpc(Pid, election)
-         end, FilteredList),
-          FilteredAnswerList = lists:filter(fun(X) -> X /= unreachable end, AnswerList),
-          if
-            length(FilteredAnswerList) > 0 ->
-              bully(PidCoordinator, Processes);
-            true ->
-              lists:foreach(fun(Pid) ->
-                Pid ! {coordinator, self()}
-              end, SelflessList),
-              bully(self(), Processes)
-          end
+          bully(self(), Processes, PidCounter)
       end;
     {coordinator, PidNewCoordinator} ->
       io:format("New Coordinator ~p elected for process ~p. ~n", [PidNewCoordinator, self()]),
-      bully(PidNewCoordinator, Processes)
+      bully(PidNewCoordinator, Processes, PidCounter);
+    {PidSender, get_coordinator} ->
+      PidSender ! {self(), PidCoordinator},
+      bully(PidCoordinator, Processes, PidCounter)
   end.
 
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% Counter
+%%%%%%%%%%%%%%%%%%%%%%%%
+counter()->
+  counter(0).
+
+counter(Num)->
+  receive
+    {PidSender, election}->
+      counter(Num+1);
+    {PidSender, get_value}->
+      PidSender ! {self(), Num},
+      counter(Num)
+  end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %% Aux
@@ -92,9 +110,26 @@ rpc(Pid, Request) ->
     unreachable
   end.
 
+get_final_coordinator(Processes)->
+  lists:foreach(fun(Pid) ->
+    Coordinator = rpc(Pid, get_coordinator),
+    io:format("Process ~p final Coordinator is ~p. ~n", [Pid, Coordinator])
+    end, Processes).
 
 start_election(Processes)->
   PidProcess = lists:nth(rand:uniform(length(Processes)),Processes),
   io:format("Process ~p starts election. ~n",[PidProcess]),
   PidProcess ! {self(), election},
-  ok.
+  receive
+    {PidProcess, State} ->
+      ok
+  end.
+
+get_final_counter(Pid)->
+  Pid ! {self(), get_value},
+  receive
+    {Pid, Value}->
+      io:format("~p election messages were exchanged. ~n",[Value])
+    after 500 ->
+      timeout
+  end.
